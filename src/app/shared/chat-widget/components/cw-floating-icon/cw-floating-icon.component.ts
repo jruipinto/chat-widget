8fc0326da7ed12ccb7f12435192af0fd2b988ed0
jruipinto <44852805+jruipinto@users.noreply.github.com>
2020-01-28 14:05:12 +0000

    refactor: development

--- src/app/shared/chat-widget/components/cw-floating-icon/cw-floating-icon.component.ts
+++ src/app/shared/chat-widget/components/cw-floating-icon/cw-floating-icon.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
+import { CwStateService } from '../../services/cw-state.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cw-floating-icon',
   template: `
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-    <path fill="none" d="M0 0h24v24H0V0z" />
-    <path d="M22 2H2.01L2 22l4-4h16V2zM9 11H7V9h2v2zm4 0h-2V9h2v2zm4 0h-2V9h2v2z" />
-  </svg>
+  <div class="floating-icon" [ngClass]="{'scale-up-center': unreadMessages}">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path fill="none" d="M0 0h24v24H0V0z" />
+      <path d="M22 2H2.01L2 22l4-4h16V2zM9 11H7V9h2v2zm4 0h-2V9h2v2zm4 0h-2V9h2v2z" />
+    </svg>
+  </div>
   `,
   styleUrls: ['./cw-floating-icon.component.scss']
 })
-export class CwFloatingIconComponent {
+export class CwFloatingIconComponent implements AfterContentInit {
+  unreadMessages = false;
+
+  constructor(private cws: CwStateService) { }
+
+  ngAfterContentInit(): void {
+    this.cws.state$.pipe(
+      tap(() => this.unreadMessages = false),
+      tap(cwState => {
+        cwState.chatsPreview.forEach(chatPreview => {
+          if (chatPreview.state === 'unread') {
+            this.unreadMessages = true;
+          }
+        });
+      })
+    ).subscribe();
+  }
 }
